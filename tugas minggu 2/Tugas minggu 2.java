5d8681e74e0eabefbfdbb479dd05c3aabd1ca09b
sandirico775 <80776333+sandirico775@users.noreply.github.com>
2021-03-16 23:43:15 +0700

    Create Tugas minggu 2.java

---
+++ tugas minggu 2/Tugas minggu 2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.wdsi;
+
+/**
+ *
+ * @author DELL
+ */
+public class TugasWdsi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String FName = "muh duwi niki arisandi";
+        String IName = "E41200519";
+        String stuStatus = "active";
+        
+        
+          System.out.println("Student Name =" +FName);
+          System.out.println ("Student ID =" +IName);
+          System.out.println ("Student Status ="+stuStatus);
+    }
+    
+}
